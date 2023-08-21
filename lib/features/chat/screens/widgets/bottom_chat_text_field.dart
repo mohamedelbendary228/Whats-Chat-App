671dd434d32c3046a080996f970d6ff916b31e35
Mohamed Elbendary <albendary.com@gmail.com>
2023-08-21 23:49:30 +0300

    start implement show emoji picker

--- lib/features/chat/screens/widgets/bottom_chat_text_field.dart
+++ lib/features/chat/screens/widgets/bottom_chat_text_field.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 
+import 'package:emoji_picker_flutter/emoji_picker_flutter.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:whats_chat_app/colors.dart';
@@ -26,6 +27,7 @@ class _BottomChatTextFieldState extends ConsumerState<BottomChatTextField> {
   }
 
   bool isSendButtonVisible = false;
+  bool isEmojiPickerVisible = false;
 
   void toggleSendButton(String val) {
     if (val.isNotEmpty) {
@@ -63,6 +65,32 @@ class _BottomChatTextFieldState extends ConsumerState<BottomChatTextField> {
     }
   }
 
+  void hideEmojiPicker() {
+    setState(() {
+      isEmojiPickerVisible = false;
+    });
+  }
+
+  void showEmojiPicker() {
+    setState(() {
+      isEmojiPickerVisible = true;
+    });
+  }
+
+  void showKeyboard() {}
+
+  void hideKeyboard() {}
+
+  void toggleEmojiPicker() {
+    if (isEmojiPickerVisible) {
+      showKeyboard();
+      hideEmojiPicker();
+    } else {
+      hideKeyboard();
+      showEmojiPicker();
+    }
+  }
+
   Future<void> sendFileMessage(File file, MessageEnum messageEnum) async {
     await ref.read(chatControllerProvider).sendFileMessage(
         context: context,
@@ -74,84 +102,98 @@ class _BottomChatTextFieldState extends ConsumerState<BottomChatTextField> {
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.sizeOf(context);
-    return Row(
+    return Column(
       children: [
-        Expanded(
-          child: TextField(
-            controller: messageController,
-            onChanged: toggleSendButton,
-            decoration: InputDecoration(
-              filled: true,
-              fillColor: AppColors.mobileChatBoxColor,
-              prefixIcon: SizedBox(
-                width: size.width * 0.25,
-                child: Row(
-                  children: [
-                    IconButton(
-                      padding: EdgeInsets.zero,
-                      onPressed: () {},
-                      icon: const Icon(
-                        Icons.emoji_emotions,
-                        color: Colors.grey,
-                      ),
+        Row(
+          children: [
+            Expanded(
+              child: TextField(
+                controller: messageController,
+                onChanged: toggleSendButton,
+                decoration: InputDecoration(
+                  filled: true,
+                  fillColor: AppColors.mobileChatBoxColor,
+                  prefixIcon: SizedBox(
+                    width: size.width * 0.25,
+                    child: Row(
+                      children: [
+                        IconButton(
+                          padding: EdgeInsets.zero,
+                          onPressed: toggleEmojiPicker,
+                          icon: const Icon(
+                            Icons.emoji_emotions,
+                            color: Colors.grey,
+                          ),
+                        ),
+                        IconButton(
+                          padding: EdgeInsets.zero,
+                          onPressed: () {},
+                          icon: const Icon(
+                            Icons.gif,
+                            color: Colors.grey,
+                          ),
+                        ),
+                      ],
                     ),
-                    IconButton(
-                      padding: EdgeInsets.zero,
-                      onPressed: () {},
-                      icon: const Icon(
-                        Icons.gif,
-                        color: Colors.grey,
-                      ),
+                  ),
+                  suffixIcon: SizedBox(
+                    width: size.width * 0.25,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        IconButton(
+                          padding: EdgeInsets.zero,
+                          onPressed: selectImage,
+                          icon: const Icon(
+                            Icons.camera_alt,
+                            color: Colors.grey,
+                          ),
+                        ),
+                        IconButton(
+                          padding: EdgeInsets.zero,
+                          onPressed: selectVideo,
+                          icon: const Icon(
+                            Icons.attach_file,
+                            color: Colors.grey,
+                          ),
+                        ),
+                      ],
                     ),
-                  ],
+                  ),
+                  hintText: 'Type a message!',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20.0),
+                    borderSide: const BorderSide(
+                      width: 0,
+                      style: BorderStyle.none,
+                    ),
+                  ),
+                  contentPadding: const EdgeInsets.all(10),
                 ),
               ),
-              suffixIcon: SizedBox(
-                width: size.width * 0.25,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    IconButton(
-                      padding: EdgeInsets.zero,
-                      onPressed: selectImage,
-                      icon: const Icon(
-                        Icons.camera_alt,
-                        color: Colors.grey,
-                      ),
-                    ),
-                    IconButton(
-                      padding: EdgeInsets.zero,
-                      onPressed: selectVideo,
-                      icon: const Icon(
-                        Icons.attach_file,
-                        color: Colors.grey,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              hintText: 'Type a message!',
-              border: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(20.0),
-                borderSide: const BorderSide(
-                  width: 0,
-                  style: BorderStyle.none,
-                ),
-              ),
-              contentPadding: const EdgeInsets.all(10),
             ),
+            Padding(
+              padding: const EdgeInsets.only(bottom: 8, right: 5, left: 8),
+              child: CircleAvatar(
+                backgroundColor: AppColors.sendMessageButtonColor,
+                radius: 25,
+                child: GestureDetector(
+                    onTap: sendTextMessage,
+                    child: Icon(isSendButtonVisible ? Icons.send : Icons.mic)),
+              ),
+            )
+          ],
+        ),
+        SizedBox(
+          height: size.height * 0.36,
+          child: EmojiPicker(
+            onEmojiSelected: (category, emoji) {
+              setState(() {
+                messageController.text = messageController.text + emoji.emoji;
+              });
+            },
           ),
         ),
-        Padding(
-          padding: const EdgeInsets.only(bottom: 8, right: 5, left: 8),
-          child: CircleAvatar(
-            backgroundColor: AppColors.sendMessageButtonColor,
-            radius: 25,
-            child: GestureDetector(
-                onTap: sendTextMessage,
-                child: Icon(isSendButtonVisible ? Icons.send : Icons.mic)),
-          ),
-        )
       ],
     );
   }
